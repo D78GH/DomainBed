a9decce29fccac3ef120d2bf3246ba1a80bedce3
Julie Phan <jp@icarus.local>
2026-08-07 06:28:59 +0100

    Adding FishMLPMCL into hparam_registry.py

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -67,6 +67,12 @@ def _hparams(algorithm, dataset, random_seed):
 
     elif algorithm == 'Fish':
         _hparam('meta_lr', 0.5, lambda r:r.choice([0.05, 0.1, 0.5]))
+    
+    elif algorithm == 'FishMLPMCL':
+        _hparam('meta_lr',0.5,lambda r:r.choice([0.05,0.1,0.5]))
+        _hparam('num_prototypes',3,lambda r:r.choice([2,3,5]))
+        _hparam('proto_weight',1.0,lambda r:r.choice([0.1,0.5,1.0]))
+        _hparam('mem_weight',0.1,lambda r:r.choice([0.01,0.05,0.1]))
 
     elif algorithm == "RDM": 
         if dataset in ['DomainNet']: 
